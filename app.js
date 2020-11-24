2fd67adc2cda4cbb15c5b76e24ecc379c6e325db
bmarshreal <bmarshreal@yahoo.com>
2020-11-23 20:33:07 -0600

    integrating mySQL

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
-const path = require('path')
+const path = require("path");
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+const app = express();
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminData.routes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' })
-})
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
-app.listen(3000)
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
+// Alternative to code above --> User.hasMany(Product)
+
+sequelize
+  //.sync({ force: true })
+  .sync()
+  .then((result) => {
+    return User.findByPk(1);
+    //console.log(result);
+    app.listen(3000);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Blake", email: "bla@bla.com" });
+    }
+    return Promise.resolve(user);
+  })
+  .then((user) => {
+    //console.log(user);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { INTEGER } = require("sequelize");
+const Sequelize = require("sequelize");
+const { model } = require("../util/database");
+
+const sequelize = require("../util/database");
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
